d96e386beb748f5e1e57e01f804890a2cbce9ebc
Perl0ovkA <104366771+Perl0ovkA@users.noreply.github.com>
2024-07-11 14:37:52 +0300

    Добавьте файлы проекта.

---
+++ SkillBoxTask17.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+class Vector
+{
+public:
+	Vector()
+	{}
+
+	Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+	{}
+
+	void Show()
+	{
+		cout << "\n" << x << " " << y << " " << z;
+	}
+private:
+	double x = 0;
+	double y = 0;
+	double z = 0;
+};
+
+int main()
+{
+	Vector v;
+	v.Show();
+}
